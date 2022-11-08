a4e9856cc082a7d7ffe9155998bf3f73ac6d72f4
Abrar Khan <heyabrarkhan@gmail.com>
2022-11-08 17:31:48 +0530

    AllRoutes some routes added

--- your-store-app/src/App.js
+++ your-store-app/src/App.js
@@ -1,17 +1,10 @@
-import AbrarTest from './Abrar/Components/Test';
+import SkinPage from './Abrar/Pages/SkinPage';
 import './App.css';
-import ApurbhaTest from './Apurba/Componets/Test';
-import DibyanshuTest from './Dibyanshu/Components/Test';
-import SonaliTest from './Sonali/Components/Test';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome To YOURSTORE.COM</h1>
-      <AbrarTest/>
-      <ApurbhaTest/>
-      <DibyanshuTest/>
-      <SonaliTest/>
+      <SkinPage/>
     </div>
   );
 }
