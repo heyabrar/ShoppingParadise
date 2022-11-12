176e9112cb66118bbfa9b4d121d5c012e282df28
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-11-12 22:18:37 +0000

    Added: MyWishlist Page ✨

--- your-store-app/src/Apurba/routes/ProfileRoutes.jsx
+++ your-store-app/src/Apurba/routes/ProfileRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import MyProfile from "../components/myProfile/MyProfile";
 import MyWallet from "../components/myWallet/MyWallet";
 import MyOrders from "../components/myOrders/MyOrders";
+import MyWishlist from "../components/myWishlist/MyWishlist";
 
 const ProfileRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const ProfileRoutes = () => {
       <Route path='/MyProfile' element={<MyProfile />} />
       <Route path='/wallet' element={<MyWallet />} />
       <Route path='/orders' element={<MyOrders />} />
+      <Route path='/wishlist' element={<MyWishlist />} />
     </Routes>
   );
 };
