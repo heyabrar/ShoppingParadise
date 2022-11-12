b7916eef8a13f6542e837e7ed59de755e2e9642b
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-11-12 23:00:08 +0000

    Added: MyPayments Page 🔥

--- your-store-app/src/Apurba/routes/ProfileRoutes.jsx
+++ your-store-app/src/Apurba/routes/ProfileRoutes.jsx
@@ -4,6 +4,7 @@ import MyProfile from "../components/myProfile/MyProfile";
 import MyWallet from "../components/myWallet/MyWallet";
 import MyOrders from "../components/myOrders/MyOrders";
 import MyWishlist from "../components/myWishlist/MyWishlist";
+import MyPayments from "../components/myPayments/MyPayments";
 
 const ProfileRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const ProfileRoutes = () => {
       <Route path='/wallet' element={<MyWallet />} />
       <Route path='/orders' element={<MyOrders />} />
       <Route path='/wishlist' element={<MyWishlist />} />
+      <Route path='/payments' element={<MyPayments />} />
     </Routes>
   );
 };
