afe55bf1ba0265ae4b6d083b3bbc823548b8fefd
Abrar Khan <heyabrarkhan@gmail.com>
2022-11-12 20:15:51 +0530

    Admin Side Working

--- your-store-app/src/Abrar/AdminSide/AdminSide.jsx
+++ your-store-app/src/Abrar/AdminSide/AdminSide.jsx
@@ -10,16 +10,9 @@ export default function AdminSide ( ) {
     const [Aprice,setAPrice] = useState('')
     const [discount,setDiscount] = useState('')
     const [category,setCategory] = useState('')
-    const [brand,setBrand] = useState('')
-
-    const [patchid,setPatchid] = useState('')
-    const [ptitle,psetTitle] = useState('')
-    const [purl,psetUrl] = useState('')
-    const [pprice,psetPrice] = useState('')
-    const [pAprice,psetAPrice] = useState('')
-    const [pdiscount,psetDiscount] = useState('')
-    const [pcategory,psetCategory] = useState('')
-    const [pbrand,psetBrand] = useState('')
+    const [brand,setBrand] = useState('');
+    const [patch,setPatch] = useState('');
+    const [patchID,setPatchID] = useState('');
     const [Delete,SetDelete] = useState('')
     const Toast  = useToast( );
 
@@ -59,19 +52,12 @@ export default function AdminSide ( ) {
         })
     };
 
-    const handlePatch = (t,u,p,ap,d,c,b,patchid) =>{
-        const payload = {
-            title : t,
-            image : u,
-            price : p,
-            actualPrice : ap,
-            discount : d,
-            category : c,
-            brand : b
-        }
+    const handlePatch = (patchID,patch) =>{
+        const payload = patch;
         Toast({position : 'bottom',duration: 2000 ,render: ( )  => (<Box p={5} bg='#fc2779' color='white' borderRadius='10px' fontWeight='600'>Patch Successful</Box>)})
-        return axios.patch(`https://adminside-yourstore.onrender.com/Products/${patchid}`, payload)
+        return axios.patch(`https://adminside-yourstore.onrender.com/Products/${patchID}`, payload)
         .then((res)=>{
+            console.log(res.data)   
         })
     }
 
@@ -127,45 +113,12 @@ export default function AdminSide ( ) {
         </Flex>
 
         <Text textAlign='center' mt='3%' fontSize='22px'>Patch</Text>
-        <Flex border='5px solid #fc2779' width={{base : '95%', md : '90%', lg : '85%'}} m='auto' gap={{base : '5px', md :'20px' , lg : '40px'}} direction={{base : 'column', md: 'row'}} padding='20px' borderRadius='10px' shadow='md'>
-        <Box  w={{base : '90%', md : '80%', lg : '50%'}} m='auto'>
-            <Text>Product ID</Text>
-            <Input placeholder="Enter ID" value={patchid} onChange={(e)=>setPatchid(e.target.value)}/>
-            <Text mt='10px'>Title</Text>
-            <Input placeholder="Enter Title" value={ptitle} onChange={(e)=>psetTitle(e.target.value)}/>
-            <Text mt='10px'>Image URL</Text>
-            <Input placeholder="Enter Image URL" value={purl} onChange={(e)=>psetUrl(e.target.value)}/>
-            <Text mt='10px'>Price</Text>
-            <Input placeholder="Enter Price" value={pprice} onChange={(e)=>psetPrice(e.target.value)}/>
-            <Text mt='10px'>Actual Price</Text>
-            <Input placeholder="Enter Actual Price" value={pAprice} onChange={(e)=>psetAPrice(e.target.value)}/>
-           </Box>
 
-           <Box w={{base : '90%', md : '80%', lg : '50%'}} m='auto' >
-            <Text mt='10px'>Discount</Text>
-            <Input placeholder="Enter Discount" value={pdiscount} onChange={(e)=>psetDiscount(e.target.value)}/>
-            <Text mt='10px'>Category</Text>
-            <Select defaultValue={'-'} value={pcategory} onChange={(e) => psetCategory(e.target.value)}>
-                <option value="--">--</option>
-                <option value="skin">Skin</option>
-                <option value="hair">Hair</option>
-                <option value="men">Men</option>
-                <option value="fragrance">Fragrance</option>
-            </Select>
-
-            <Text mt='10px'>Brand</Text>
-            <Select value={pbrand} onChange={(e)=>psetBrand(e.target.value)} defaultValue={'--'}>
-                <option value="--">--</option>
-                <option value="dotkey">Dot & Key</option>
-                <option value="skinras">Skin Ras</option>
-                <option value="loreal">Loreal</option>
-                <option value="estee">Estee Lauder</option>
-                <option value="garnier">Garnier</option>
-            </Select>
-            <br /> <br />
-            <Button onClick={( ) => handlePatch(ptitle,purl,pprice,pAprice,pdiscount,pcategory,pbrand,patchid)} color='white' bg='#fc2779' colorScheme='#fc2779'>Patch</Button>
+        <Box>
+            <Input placeholder="Enter Product ID" value={patchID} onChange={(e) => setPatchID(e.target.value)}/>
+            <Input placeholder="Enter Data" value={patch} onChange={(e) => setPatch(e.target.value)}/>
+            <Button onClick={( ) => handlePatch(patchID,patch)}>Patch</Button>
         </Box>
-        </Flex>
 
         <Text textAlign='center' fontSize='22px' mt='3%'>Delete</Text>
         <Box border='5px solid #fc2779' width={{base : '80%' ,md : '40%', lg : '20%'}} m='auto' padding='20px' borderRadius='10px' shadow='lg'>
