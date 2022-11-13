b463c6e48ff6d9722b4c77927f9f10b00b182883
Abrar Khan <heyabrarkhan@gmail.com>
2022-11-13 14:02:00 +0530

    image center in mom and baby

--- your-store-app/src/Sonali/Components/MomBabyTop.jsx
+++ your-store-app/src/Sonali/Components/MomBabyTop.jsx
@@ -4,7 +4,7 @@ export default function MomBabayTop (){
     return(
         <>
          <Box>
-            <Image src="https://images-static.nykaa.com/uploads/f36c4f0c-3d99-4cd4-bb31-d32220ea3e9c.jpg?tr=w-1200,cm-pad_resize"/>
+            <Image m='auto' mt='20px'  src="https://images-static.nykaa.com/uploads/f36c4f0c-3d99-4cd4-bb31-d32220ea3e9c.jpg?tr=w-1200,cm-pad_resize"/>
          </Box>
         </>
     )
