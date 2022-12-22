075d5edd56a8b29b649f06ef31c2fb29129136cf
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-12-22 04:37:17 +0000

    Controller Added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { regUser, loginUser } = require("../controllers/user.controller");
 const router = express.Router();
 
-router.post("/register");
-router.post("/login");
+router.post("/register", regUser);
+router.post("/login", loginUser);
 
 module.exports = router;
