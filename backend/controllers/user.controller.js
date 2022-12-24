13286e56bcbdf6cc692d9f32d3a9464e9ee56688
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-12-24 05:19:58 +0000

    Controller Fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,7 +12,15 @@ const regUser = asyncHandler(async (req, res, next) => {
   const user = await User.create({ name, email, password, phone, picture });
 
   if (user) {
-    res.status(201).send(user);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      isAdmin: user.isAdmin,
+      picture: user.picture,
+      token: genToken(user._id),
+    });
   } else {
     res.status(500).send("Failed to create user");
   }
@@ -22,8 +30,16 @@ const loginUser = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   const hashedPassword = await user.matchPassword(password);
-  if (userExists && hashedPassword) {
-    res.status(200).send(user);
+  if (user && hashedPassword) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      isAdmin: user.isAdmin,
+      picture: user.picture,
+      token: genToken(user._id),
+    });
   } else {
     res.status(400).send("Invalid email or password");
   }
